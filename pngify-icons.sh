ab74073dea0a0018fc5e2e305feb5b47fff3256d
mroemer <mroemer@inovex.de>
2018-02-12 14:06:38 +0100

    also generate icon in 64x64 (for listing on AMO)

--- pngify-icons.sh
+++ pngify-icons.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 inkscape -z -e ./src/images/icon-128.png -w 128 -h 128 ./src/images/icon.svg
+inkscape -z -e ./src/images/icon-64.png -w 64 -h 64 ./src/images/icon.svg
 inkscape -z -e ./src/images/icon-48.png -w 48 -h 48 ./src/images/icon.svg
 inkscape -z -e ./src/images/icon-32.png -w 32 -h 32 ./src/images/icon.svg
 inkscape -z -e ./src/images/icon-16.png -w 16 -h 16 ./src/images/icon.svg
